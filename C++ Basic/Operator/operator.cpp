d77562844b7b60981b14f1ae331f6e17aa41af27
sameerkumarmeher <sameermeher005@gmail.com>
2026-02-24 16:14:33 +0530

    my first code

---
+++ C++ Basic/Operator/operator.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Arithmetic Operator
+    /* int x=10, y=3;
+    cout << "Addition : " << x+y << endl;
+    cout << "Substraction : " << x-y << endl;
+    cout << "Multiplication : " << x*y << endl;
+    cout << "Division : " << x/y << endl;
+    cout << "Modulo : " << x%y << endl; */
+
+
+    //Post-increment and pre-increment
+
+    /*int x=10;
+    int y;
+    y = ++x;
+    cout << x << endl << y << endl; // x = 11 , y = 11
+    y = x++;
+    cout << x << endl << y << endl; // x = 12 , y = 11*/
+
+    // Relational Operator if 
+    // If Condition is TRUE then return 1 and if Condition is FALSE then return 0
+    int x=3,y=6;
+    cout << "x < y : " << (x < y) << endl;
+    cout << "x > y : " << (x > y) << endl;
+    cout << "x <= y : " << (x <= y) << endl;
+    cout << "x >= y : " << (x >= y) << endl;
+    cout << "x == y : " << (x == y) << endl;
+    cout << "x != y : " << (x != y) << endl;
+    return 0;
+
+}
